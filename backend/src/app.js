4b53426aa5a1fae771447089828c5f25579a39da
prometeh <prometeh.zanjir@gmail.com>
2022-08-31 12:56:09 +0200

    backend: connected to the mongo databese

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const connectToMongo = require("./database/mongo/connect");
+
 const express = require("express");
 const { StatusCodes } = require("http-status-codes");
 
@@ -10,4 +13,14 @@ app.get("*", (req, res) => {
   res.status(StatusCodes.NOT_FOUND).send("<h1>404: Page Not Found.</h1>");
 });
 
-app.listen(port, () => console.log("server is listening on port:" + port));
+const start = async () => {
+  try {
+    await connectToMongo(process.env.MONGO_URI);
+    console.log("connected to MongoDB successfully!");
+    app.listen(port, () => console.log("server is listening on port:" + port));
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+start();
