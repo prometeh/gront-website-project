fc4147bb6e9a4004dad609ad12d4d6426630050e
Radha Manickam <radhaarul@gmail.com>
2022-09-13 15:13:07 +0200

    Backend: created News model

---
+++ backend/src/models/News.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const NewsSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    media:[String],
+    article: {
+      type: [String],
+      required: [true, "Article is required"],
+    },
+    createdDate:{
+        type: Date,
+        default: Date.now(),
+    },
+    modifiedDate:{
+        type: Date,
+
+    }
+  });
+
+  module.exports = mongoose.model("News", NewsSchema);
