f80a7a9efa29511e4bfb4c24cd11debd4d5c49eb
Sajid Minhas <sajid.minhas@lexutb.se>
2022-09-15 13:12:30 +0200

    backend: get news api

--- backend/src/controllers/news.js
+++ backend/src/controllers/news.js
@@ -7,6 +7,7 @@ const create = async (req, res) => {
   res.status(StatusCodes.CREATED).send({ msg: "created the news" });
 };
 
+// Deleting news api request
 const deleteNews = async (req, res) => {
   const {
     params: { id: newsId },
@@ -21,4 +22,20 @@ const deleteNews = async (req, res) => {
   res.status(StatusCodes.OK).send({ msg: "News has been deleted successfuly" });
 };
 
-module.exports = { create, deleteNews };
+// getting single news api request
+
+const getNews = async (req, res) => {
+  const {
+    params: { id: newsId },
+  } = req;
+  const news = await News.findById({ _id: newsId });
+  if (!news) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `There is no news with id: ${newsId}` });
+  }
+
+  res.status(StatusCodes.OK).json({ news });
+};
+
+module.exports = { create, deleteNews, getNews };
--- backend/src/routes/news.js
+++ backend/src/routes/news.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { create, deleteNews } = require("../controllers/news");
+const { create, deleteNews, getNews } = require("../controllers/news");
 const router = express.Router();
 
 router.post("/create", create);
 router.delete("/delete/:id", deleteNews);
+router.get("/get/:id", getNews);
 
 module.exports = router;
