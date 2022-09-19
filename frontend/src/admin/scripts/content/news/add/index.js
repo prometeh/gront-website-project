f10a4537799e244916b7abf1974cc9cc053d049d
Kajal Parekh <kajal.parekh@lexutb.se>
2022-09-19 15:53:41 +0200

    frontend addnews

--- frontend/src/admin/scripts/content/news/add/index.js
+++ frontend/src/admin/scripts/content/news/add/index.js
@@ -19,31 +19,31 @@ const addButtonEvents = () => {
   const saveButton = document.getElementById("dashboard-save");
   const cancelButton = document.getElementById("dashboard-cancel");
   
-       saveButton.addEventListener("click", () => {
-         // TODO: add my functionality 
-        return undefined;
+  saveButton.addEventListener("click", () => {
+    // TODO: add my functionality 
+    return undefined;
   });
 
-       cancelButton.addEventListener("click", () => {
-          // TODO: add my functionality
-        return undefined;
+  cancelButton.addEventListener("click", () => {
+    // TODO: add my functionality
+    return undefined;
   });
 };
-      // we update the main content we want to show
-        const updateContentPage = (contentPage) => {
-        const save =
+// we update the main content we want to show
+const updateContentPage = (contentPage) => {
+  const save =
                     "<strong>Save:</strong><br> By clicking on this button, you can save the added news.";
-        const cancel =
+  const cancel =
                     "<strong>Cancel:</strong><br>By clicking on this button, you can cancel the changes for added news.";
 
   mainElement.clearContents(contentPage);
-  mainElement.addParagraph(savePage, save);
-  mainElement.addParagraph(cancelPage, cancel);
+  mainElement.addParagraph(contentPage, save);
+  mainElement.addParagraph(contentPage, cancel);
 };
 const render = () => {
   addNavMenu(navButtons);
   addButtonEvents();
-  
+  updateContentPage(contentPage);
 };
 
 module.exports = { render };
