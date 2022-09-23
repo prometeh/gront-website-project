a0a7f35c0969f3942b1eaa4d959b1bb95eb48095
Kajal Parekh <kajal.parekh@lexutb.se>
2022-09-23 10:47:42 +0200

    frontend: added the confimation for add button

--- frontend/src/admin/scripts/content/news/add/index.js
+++ frontend/src/admin/scripts/content/news/add/index.js
@@ -9,46 +9,59 @@ const navButtons = document.getElementById("nav-buttons");
 
 const contentPage = document.getElementById("main-content");
 
-const addNavMenu = (buttonsList) => {
+const addSaveMenu = (buttonsList) => {
   navMenu.removeButtons(buttonsList);
   navMenu.addButton(buttonsList, "dashboard-save", "Save");
   navMenu.addButton(buttonsList, "dashboard-cancel", "Cancel");
 };
 
-const addButtonEvents = () => {
+const addSaveButtonEvents = () => {
   const saveButton = document.getElementById("dashboard-save");
   const cancelButton = document.getElementById("dashboard-cancel");
 
-  saveButton.addEventListener("click", async (e) => {
+  saveButton.addEventListener("click", () => {
+    addConfirmMenu(navButtons);
+    confirmButtonEvents();
+  });
+
+  cancelButton.addEventListener("click", () => {
+    const cancelPage = require("./..");
+    cancelPage.render();
+  });
+};
+
+const addConfirmMenu = (buttonsList) => {
+  navMenu.removeButtons(buttonsList);
+  navMenu.addButton(buttonsList, "dashboard-yes", "Yes");
+  navMenu.addButton(buttonsList, "dashboard-no", "No");
+};
+
+const confirmButtonEvents = () => {
+  const yesButton = document.getElementById("dashboard-yes");
+  const noButton = document.getElementById("dashboard-no");
+
+  yesButton.addEventListener("click", async (e) => {
     const title = document.getElementById("title");
     const article = document.getElementById("article");
     const media = document.getElementById("media");
 
     e.preventDefault();
     try {
-      const response = await axios.post(
-        "/api/v1/news/create",
-        {
-          title: title.value,
-          article: article.value,
-          media: media.value,
-        },
-        {
-          withCredentials: true,
-          headers: {
-            authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        }
-      );
-      console.log(response);
+      await axios.post("/api/v1/news/create", {
+        title: title.value,
+        article: article.value,
+        media: media.value,
+      });
+
+      const newsPage = require(".././index");
+      newsPage.render();
     } catch (err) {
       console.log(err);
     }
   });
 
-  cancelButton.addEventListener("click", () => {
-    const dashboard = require("./..");
-    dashboard.render();
+  noButton.addEventListener("click", () => {
+    saveMenuRender();
   });
 };
 // we update the main content we want to show
@@ -58,8 +71,12 @@ const updateContentPage = (contentPage) => {
 };
 const render = () => {
   updateContentPage(contentPage);
-  addNavMenu(navButtons);
-  addButtonEvents();
+  saveMenuRender();
+};
+
+const saveMenuRender = () => {
+  addSaveMenu(navButtons);
+  addSaveButtonEvents();
 };
 
 module.exports = { render };
