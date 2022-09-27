a066fead3da16fdfbf112fd73de0c6f6854c0b08
Ushi <ushita.mohammadi@gmail.com>
2022-09-27 12:13:59 +0200

    frontend: added the list component for news

--- frontend/src/admin/scripts/content/news/index.js
+++ frontend/src/admin/scripts/content/news/index.js
@@ -1,10 +1,8 @@
-const axios = require("axios").default;
 const navMenu = require("./../../components/navMenu");
 const mainElement = require("./../../components/mainElement");
+const ulNews = require("./../../components/news/list");
+
 const navButtons = document.getElementById("nav-buttons");
-
-// then we set the parts we want to update
-
 const newsPage = document.getElementById("main-content");
 
 const updateNavMenu = (buttonsList) => {
@@ -22,13 +20,13 @@ const updateButtonEvents = () => {
   const backButton = document.getElementById("dashboard-back");
 
   backButton.addEventListener("click", () => {
-    const dashboard = require("./..");
-    dashboard.render();
+    const contentPage = require("./..");
+    contentPage.render();
   });
 
   addButton.addEventListener("click", () => {
-    const addButton = require("./add");
-    addButton.render();
+    const addPage = require("./add");
+    addPage.render();
   });
 
   editButton.addEventListener("click", () => {
@@ -37,17 +35,17 @@ const updateButtonEvents = () => {
   });
 
   deleteButton.addEventListener("click", () => {
-    const deleteNews = require("./delete");
-    deleteNews.render();
+    const deletePage = require("./delete");
+    deletePage.render();
   });
 };
-// we update the main content we want to show
+
 const updateContentPage = (newsPage) => {
   const title = "News";
   const add =
-    "<br><strong>Add:</strong><br>By clicking the add button you will be redirected to a form, where you have the option to add title and description of your news and these two fields are required. There is an other field media and that is optional but if you want to add any image and video with the news so you can upload file here. ";
+    "<br><strong>Add:</strong><br>By clicking the add button you will be redirected to a form, where you have the option to add title and description of your news and these two fields are required. There is an other field media and that is optional but if you want to add any image and video with the news so you can upload file here.<br> ";
   const edit =
-    "<strong>Edit:</strong><br>Here you have a list of all the existing news and you will select which one needs to be updated. News will open in a form there you can change text and media file and click save button to apply all the changes.";
+    "<strong>Edit:</strong><br>Here you have a list of all the existing news and you will select which one needs to be updated. News will open in a form there you can change text and media file and click save button to apply all the changes.<br>";
   const deleteNews =
     "<strong>Delete:</strong><br>By clicking the delete button there you have a complete list of news and each news has a check box. If you want to delete any news from a list, you need to select that news check box and press delete button then reconfirm by Yes or No. After you click Yes, news will be removed from the website.<br><br>";
 
@@ -56,17 +54,10 @@ const updateContentPage = (newsPage) => {
   mainElement.addParagraph(newsPage, add);
   mainElement.addParagraph(newsPage, edit);
   mainElement.addParagraph(newsPage, deleteNews);
-  updateList(newsPage);
+  ulNews.addNewsList(newsPage, "normal");
 };
 
 // this is responsible to show list of all the news
-const updateList = async (to) => {
-  const { data } = await axios.get("/api/v1/news/get");
-  const selectedData = data.news.map(
-    (n) => `${n.title}: ${n.article}: ${n.media}`
-  );
-  mainElement.addUnorderedList(to, selectedData);
-};
 
 // this function is the only one we export
 // this is responsible for rendering each page
