46f064379b23bc0605c35c7640589af6c95bf5c9
Radha Manickam <radhaarul@gmail.com>
2022-10-04 09:48:26 +0200

    frontend:accordion in list

--- frontend/src/admin/scripts/components/news/list.js
+++ frontend/src/admin/scripts/components/news/list.js
@@ -48,21 +48,55 @@ const addNewsList = async (
   const newsData = await getNews(newsId);
   let newsListItems = new Array();
   let content;
-
+  let accordionPart1 =
+    '<div class="accordion" data-accordion="open">' +
+    '<div class="accordion-item relative mb-3">' +
+    '<h2 class="mb-0">' +
+    "<button" +
+    ' class="accordion-button text-left font-semibold"' +
+    ' type="button"' +
+    'aria-expanded="false"' +
+    ">" +
+    "<span>";
+  let accordionPart2 =
+    "</span>" +
+    '<i class="fa fa-plus collapse-close absolute right-0 pt-1 text-xs"></i>' +
+    '<i class="fa fa-minus collapse-open absolute right-0 pt-1 text-xs"></i>' +
+    "</button>" +
+    "</h2>" +
+    '<div  class="show">' +
+    '<div class="py-4 text-sm opacity-60">';
+  let accordionPart3 = "</div>" + "</div>" + "</div>" + "</div>";
   if (!newsId) {
     newsData.forEach((news) => {
       if (listType === "normal") {
-        content = news.title;
+        //content = news.title;
+        content =
+          accordionPart1 +
+          news.title +
+          accordionPart2 +
+          news.article.slice(0, 5) +
+          accordionPart3;
       } else if (listType === "checkBox") {
-        content = `<input type = "checkbox"
+        content =
+          accordionPart1 +
+          `<input type = "checkbox"
                   id = "${news.title} ${news._id}"
                   name = "${news.createdDate}">
-           <label for = "${news.title} ${news._id}"> ${news.title} </label>`;
+           <label for = "${news.title} ${news._id}"> ${news.title} </label>` +
+          accordionPart2 +
+          news.article +
+          accordionPart3;
       } else if (listType === "radio") {
-        content = `<input type = "radio"
+        content =
+          accordionPart1 +
+          `<input type = "radio"
                   id = "${news.title} ${news._id}"
                   name = "selected news">
-           <label for = "${news.title} ${news._id}"> ${news.title} </label>`;
+           <label for = "${news.title} ${news._id}"> ${news.title} </label>` +
+          accordionPart2 +
+          news.article +
+          accordionPart3;
       } else {
         content = "Invalid list item type";
       }
